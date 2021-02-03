2ae53673894118e5f4fdf4208a7fb4f1e0f05000
Nightrider0098 <hitesh08011999@gmail.com>
2021-02-03 18:22:56 +0530

    initial commit

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ const app = express();
 const bodyParser = require("body-parser");
 const port = 8080;
 const path = require('path')
+const fs = require('fs')
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-const { connection } = require("./connector");
 const cors = require('cors')
 app.use(cors())
 
